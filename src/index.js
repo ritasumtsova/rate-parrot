18ae302bc1f8ee7876c8b017fc3630d45f7c07d4
ritasumtsova <ritasumtsova@gmail.com>
2025-08-19 14:34:52 +0300

    revert hook

--- src/index.js
+++ src/index.js
@@ -5,27 +5,7 @@ import { cryptoCommands } from "./commands/crypto.js";
 
 dotenv.config();
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {
-  polling: false,
-  webHook: {
-    port: process.env.PORT,
-  },
-});
-
-bot.deleteWebHook().then(() => {
-  console.log("Old webhook deleted");
-
-  return bot.setWebHook(`${URL}/bot${TOKEN}`, {
-    secret_token: SECRET,
-  });
-}).then(() => {
-  console.log(`Webhook set to ${URL}/bot${TOKEN}`);
-}).catch(err => {
-  console.error("Webhook setup error:", err);
-});
-
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 startCommand(bot);
 cryptoCommands(bot);
-
-console.log(`Bot is running on port ${process.env.PORT}`);
