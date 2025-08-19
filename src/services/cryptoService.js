1c8e3539fc3aaf0d39df5c5a980f509c60305687
ritasumtsova <ritasumtsova@gmail.com>
2025-08-19 14:36:43 +0300

    fix

--- src/services/cryptoService.js
+++ src/services/cryptoService.js
@@ -2,5 +2,5 @@ export async function getCryptoRate(coin = "bitcoin") {
   const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`;
   const res = await fetch(url);
   const data = await res.json();
-  return data[coin].usd;
+  return data[coin]?.usd;
 }
